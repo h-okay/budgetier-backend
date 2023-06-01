bf617ec5a1247d48239f0c78a376ee88b10b619f
Hakan Okay <hokay18@gmail.com>
2023-06-01 14:45:41 -0400

    Project Init

---
+++ src/main/java/com/hokay/budgetier/BudgetierApplication.java
@@ -0,0 +1,13 @@
+package com.hokay.budgetier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hokay/budgetier/BudgetierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hokay.budgetier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
