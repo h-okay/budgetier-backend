40c4d23000c84fa2e13631f64e638cf44cbb7c6a
Hakan Okay <hokay18@gmail.com>
2023-06-01 22:38:15 -0400

    Annotate the main method as a RestController

--- src/main/java/com/hokay/budgetier/BudgetierApplication.java
+++ src/main/java/com/hokay/budgetier/BudgetierApplication.java
@@ -2,12 +2,17 @@ package com.hokay.budgetier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BudgetierApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BudgetierApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BudgetierApplication.class, args);
+    }
 
 }
+
+
+
