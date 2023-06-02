25142f6742852841b2ed99abba1376e8ea0de99b
Hakan Okay <hokay18@gmail.com>
2023-06-02 15:26:47 -0400

    Configure Mongo to remove "_class" column

--- src/main/java/com/hokay/budgetier/MongoConfiguration.java
+++ src/main/java/com/hokay/budgetier/MongoConfiguration.java
@@ -1,2 +1,26 @@
-package com.hokay.budgetier;public class MongoConfiguration {
+package com.hokay.budgetier;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class MongoConfiguration {
+
+    private final MappingMongoConverter mongoConverter;
+
+    @Autowired
+    public MongoConfiguration(MappingMongoConverter mappingMongoConverter) {
+        this.mongoConverter = mappingMongoConverter;
+    }
+
+    @PostConstruct
+    public void setUpMongoEscapeCharacterAndTypeMapperConversion() {
+        mongoConverter.setMapKeyDotReplacement("_"); // This will remove _class: key
+        mongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    }
+
 }
